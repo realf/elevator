8684c7348999d4095267c0c41e02490450e00fd7
Serhii Dunets <dunets.deve@gmail.com>
2025-12-05 11:02:35 +0000

    Create project

---
+++ Elevator/Elevator/ElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorApp.swift
+//  Elevator
+//
+//  Created by alf on 05.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Elevator/ElevatorTests/ElevatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorTests.swift
+//  ElevatorTests
+//
+//  Created by alf on 05.12.2025.
+//
+
+import Testing
+@testable import Elevator
+
+struct ElevatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
